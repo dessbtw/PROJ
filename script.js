5f469677f3868bb1c9e503867aeef2f9d519ec76
dessbtw <dessbtw@gmail.com>
2025-04-09 01:52:01 +0600

    Initial clean commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const images = document.querySelectorAll('.slider-img');
+const controlls = document.querySelectorAll('.controlls');
+let imageIndex = 0;
+
+function show(index) {
+    images[imageIndex].classList.remove('active');
+    images[index].classList.add('active');
+    imageIndex = index;
+}
+
+controlls.forEach((e) => {
+    e.addEventListener('click', () => {
+        if (event.target.classList.contains('left')) {
+            let index = imageIndex - 1;
+
+            if (index < 0) {
+                index = images.length - 1;
+            }
+
+            show(index);
+        } else if (event.target.classList.contains('right')) {
+            let index = imageIndex + 1;
+            if (index >= images.length) {
+                index = 0;
+            }
+            show(index);
+        }
+    })
+})
+
+show(imageIndex);
+
+
+
+const imbox = document.querySelectorAll(".imageblock");
+const contr = document.querySelectorAll(".contr");
+let imboxIndex = 0
+
+function showim(index) {
+    imbox[imboxIndex].classList.remove('act');
+    imbox[index].classList.add('act');
+    imboxIndex = index;
+}
+
+contr.forEach((e) => {
+    e.addEventListener('click', () => {
+        if (event.target.classList.contains('L')) {
+            let index = imboxIndex - 1;
+
+            if (index < 0) {
+                index = imboxIndex.length - 1;
+            }
+            
+            showim(index)
+        }
+        else if (event.target.classList.contains('R')) {
+            let index = imboxIndex + 1;
+            if (index >= images.length) {
+                index = 0
+            }
+            showim(index)
+        }
+    })
+})
+
+showim(imboxIndex)
